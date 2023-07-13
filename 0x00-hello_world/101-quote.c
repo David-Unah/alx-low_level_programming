94b4272f0bb31b50f330555fc30783c0d7a4f5c0
David-Unah <davidunah022@gmail.com>
2023-07-13 15:41:19 -0700

    program with error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,12 @@
-// c programming code goes in here
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: without using printf or put
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(2, message, sizeof(message) - 1);
+return (1);
+}
