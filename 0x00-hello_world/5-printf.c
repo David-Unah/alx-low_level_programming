71926dd2f208fa50855d11b80609ac4265ebefe4
David-Unah <davidunah022@gmail.com>
2023-07-13 14:25:23 -0700

    using printf in c programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-//programming code goes in here
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: using printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
