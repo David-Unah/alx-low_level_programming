c81595ffd9ee942fb317b991a5fd2ec18f9dd31d
David-Unah <davidunah022@gmail.com>
2023-07-14 16:39:44 -0700

    alphabete in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,15 @@
-//8th question
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (char c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
