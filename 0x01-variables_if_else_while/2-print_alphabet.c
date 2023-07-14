6d22160678b69e8c1690d86682199dc558b051b9
David-Unah <davidunah022@gmail.com>
2023-07-14 15:20:51 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
-//third question
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: printing in lovercase alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
