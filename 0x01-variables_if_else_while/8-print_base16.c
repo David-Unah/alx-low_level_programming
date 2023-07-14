23dc86433a7783cab7a641ebf44ebaf5b4938617
David-Unah <davidunah022@gmail.com>
2023-07-14 16:49:01 -0700

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,23 @@
-//9th question
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 0 ; n < 16 ; n++)
+{
+if (n < 10)
+{
+putchar('0' + n);
+}
+else
+{
+putchar(87 + n);
+}
+}
+putchar('\n');
+return (0);
+}
