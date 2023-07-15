95be7b26b33c39cda585fe5fdf972b7843d1d5ba
David-Unah <davidunah022@gmail.com>
2023-07-14 17:31:49 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,30 +6,23 @@
  */
 int main(void)
 {
-int tho;
-int hun;
-int ten;
-int bas;
-for (tho = 0 ; tho < 10 ; tho++)
+for (int i = 0; i <= 99; i++)
 {
-for (hun = 0 ; hun <= 10 ; hun++)
+for (int j = i; j <= 99; j++)
 {
-for (ten = 0 ; ten < 10 ; ten++)
-{
-for (bas = 0 ; bas < 10 ; bas++)
-{
-putchar('0' + tho);
-putchar('0' + hun);
-putchar(32);
-putchar('0' + ten);
-putchar('0' + bas);
-if (!(tho == 9 && hun == 8))
+int tens1 = i / 10;
+int ones1 = i % 10;
+int tens2 = j / 10;
+int ones2 = j % 10;
+putchar(tens1 + '0');
+putchar(ones1 + '0');
+putchar(' ');
+putchar(tens2 + '0');
+putchar(ones2 + '0');
+if (i != 99 || j != 99)
 {
 putchar(',');
-putchar(32);
-}
-bas++;
-}
+putchar(' ');
 }
 }
 }
