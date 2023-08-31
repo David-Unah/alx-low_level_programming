5c30e954f438e3153c1f1cdd68f53e06f8794a5e
David-Unah <davidunah022@gmail.com>
2023-08-31 09:49:34 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int p;
-	unsigned int dec_val = 0;
+	unsigned int check = 0;
 
 	if (!b)
 		return (0);
@@ -17,8 +17,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[p] < '0' || b[p] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[p] - '0');
+		check = 2 * check + (b[p] - '0');
 	}
 
-	return (dec_val);
+	return (check);
 }
